8877bb99a15a9a5d4e0d9ed4a183170e014abdc5
axlash <axlash7@naver.com>
2023-09-14 04:25:33 +0900

    FileManagerService 경로

--- src/main/java/com/bluebooks/common/FileManagerService.java
+++ src/main/java/com/bluebooks/common/FileManagerService.java
@@ -17,7 +17,7 @@ public class FileManagerService {
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	// 실제 업로드가 된 이미지가 저장될 경로(서버)
-	public static final String FILE_UPLOAD_PATH = "C:\\Users\\axlas\\Desktop\\kotaeyoung\\6_spring_project\\new_bluebooks\\workspace\\images/";
+	public static final String FILE_UPLOAD_PATH = "C:\\Users\\axlash\\Desktop\\kotaeyoung\\new_bluebooks\\workspace\\images/";
 //	public static final String FILE_UPLOAD_PATH = "D:\\kotaeyoung\\6_spring_project\\bluebooks\\workspace\\images/";
 	// 마지막에 / 붙인다.
 	
